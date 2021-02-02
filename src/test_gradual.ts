3204e36b2e7f1cf7e7dc2ab5ed21b680588783e5
Chris Griffing <cmgriffing@gmail.com>
2021-02-01 19:17:52 -0800

    fix: flip seeds for testing and add README and LICENSE

--- src/test_gradual.ts
+++ src/test_gradual.ts
@@ -2,9 +2,10 @@ import Hasher from "./hasher";
 
 let uuid = "b7e91cc5-ec76-4ec3-9c1c-075032a13a1a";
 
-let workingSeed: number = 0.22;
-let nonWorkingSeed: number = 0.11;
+let workingSeed: number = 0.11;
+let nonWorkingSeed: number = 0.22;
 
+console.log("Working Hash:", new Hasher(workingSeed).hashString(uuid));
 console.log("WORKING SEED", new Hasher(workingSeed).hashString(uuid) <= 40);
 
 console.log(
